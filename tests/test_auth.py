5286f21ea915111371bbe3631ed60c981a4e29ef
kachulio1 <jngugi88@gmail.com>
2017-06-22 11:48:05 +0300

    [Chore: #147605393] Test registration with existing email - if the user tries to register with an existing email - i also pep8tfy the whole module code

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -3,6 +3,7 @@ from flask_testing import TestCase
 from app import create_app, db
 from config import app_config
 
+
 class TestAuth(TestCase):
     def create_app(self):
         app = self.app = create_app(app_config['testing'])
@@ -11,7 +12,6 @@ class TestAuth(TestCase):
     def setUp(self):
         self.client = self.create_app().test_client()
         db.create_all()
-
 
     def test_register(self):
         payload = {
@@ -22,3 +22,36 @@ class TestAuth(TestCase):
 
         r = self.client.post('/auth/register', data=json.dumps(payload), content_type='application/json')
         self.assertEqual(r.status_code, 201)
+
+    def test_register_with_existing_username(self):
+        payload = {
+            'username': 'Kachulio',
+            'email': 'yahoo@ymail.com',
+            'password': '12jkl23@'
+        }
+        r = self.client.post('/auth/register', data=json.dumps(payload), content_type='application/json')
+        payload = {
+            'username': 'Kachulio',
+            'email': 'yahoo@ymail.com',
+            'password': '12jkl23@'
+        }
+        r = self.client.post('/auth/register', data=json.dumps(payload), content_type='application/json')
+        self.assertIn('Username has already been taken', json.loads(r.data.decode())['msg'])
+
+    def test_register_with_existing_email(self):
+        payload = {
+            'username': 'Kachulio',
+            'email': 'yahoo@ymail.com',
+            'password': '12jkl23@'
+        }
+        r = self.client.post('/auth/register', data=json.dumps(payload), content_type='application/json')
+
+        payload = {
+            'username': 'David',
+            'email': 'yahoo@ymail.com',
+            'password': '12jkl23@'
+        }
+        # make a post with an email that has already been registered with another user
+        r = self.client.post('/auth/register', data=json.dumps(payload), content_type='application/json')
+        self.assertIn('The email address you have entered is already registered', json.loads(r.data.decode())['msg'])
+
