c67784889a756ed523938c1f7e53b35d239cd77f
kachulio1 <jngugi88@gmail.com>
2017-06-23 10:05:33 +0300

    [Chore: #147692325] Add create_app function - initialized sql-alchemy - imported the view from auth and registered the blueprint

---
+++ app/__init__.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+
+# initialize sql-alchemy
+db = SQLAlchemy()
+# Setup for the Flask-JWT-Extended extension
+jwt = JWTManager()
+
+
+# function factory to create app with different settings for the current app that's being created
+def create_app(config_name):
+
+    # instantiate a WSGI application object
+    app = Flask(__name__)
+    # and get settings configurations from config.py file
+    app.config.from_object(config_name)
+
+    # initialize database object which is imported
+    # in models.py and in tests
+    db.init_app(app)
+    jwt.init_app(app)
+
+    # import auth blueprint and register it to the WSGI app
+    from app.auth import views
+    app.register_blueprint(views.auth)
+
+
+
+    return app
