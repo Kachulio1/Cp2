683738f488c81a457a22949ee9c9b2989a5b5dc3
kachulio1 <jngugi88@gmail.com>
2017-06-23 09:56:40 +0300

    [Chore: #147735331] Add config file - created a development and testing configarations

---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+class Config(object):
+    """Parent configuration class."""
+    DEBUG = False
+    CSRF_ENABLED = True
+    SECRET = os.getenv('SECRET')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+class DevelopmentConfig(Config):
+    """Configurations for Development."""
+    DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/bucket'
+class TestingConfig(Config):
+    """Configurations for Testing, with a separate test database."""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'test.sqlite')
+    DEBUG = True
+
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+
+}
